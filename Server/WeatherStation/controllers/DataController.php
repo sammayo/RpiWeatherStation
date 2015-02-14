e31a48b1836171ae07d585f540cedd79d818e78a
Sam Mayo <sammayo888@gmail.com>
2015-02-14 06:40:25 -0500

    moved dataLog to 'Server' directory

--- Server/WeatherStation/controllers/DataController.php
+++ Server/WeatherStation/controllers/DataController.php
@@ -10,7 +10,7 @@ class DataController extends Controller
     public function actionSave()
     {
 	$jsonData = Yii::$app->request->get('data');
-	$logPath = realpath(Yii::$app->basePath . '/web/assets/dataLog');
+	$logPath = realpath(Yii::$app->basePath . '/../dataLog');
 	$logFile = fopen($logPath, 'a');
 	fwrite($logFile, $jsonData);
 	fwrite($logFile, "\n");
@@ -19,14 +19,14 @@ class DataController extends Controller
 
     public function actionReset()
     {
-	$logPath = realpath(Yii::$app->basePath . '/web/assets/dataLog');
+	$logPath = realpath(Yii::$app->basePath . '/../dataLog');
 	$logFile = fopen($logPath, 'w');
 	fclose($logFile);
     }
 
     public function actionGet()
     {
-	$logPath = realpath(Yii::$app->basePath . '/web/assets/dataLog');
+	$logPath = realpath(Yii::$app->basePath . '/../dataLog');
 	$jsonArray = file($logPath);
 	foreach($jsonArray as $line_num => $line)
 	{
