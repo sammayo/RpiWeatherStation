6a4e4eefea96730ce386d22c72b5783ac0ddb71b
Sam Mayo <sammayo888@gmail.com>
2015-02-14 00:17:49 -0500

    added dataLog and DataController

---
+++ Server/WeatherStation/controllers/DataController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class DataController extends Controller
+{
+    public function actionSave()
+    {
+	$jsonData = Yii::$app->request->get('data');
+	$logPath = realpath(Yii::$app->basePath . '/web/assets/dataLog');
+	$logFile = fopen($logPath, 'a');
+	fwrite($logFile, $jsonData);
+	fwrite($logFile, "\n");
+	fclose($logFile);
+    }
+
+    public function actionReset()
+    {
+	$logPath = realpath(Yii::$app->basePath . '/web/assets/dataLog');
+	$logFile = fopen($logPath, 'w');
+	fclose($logFile);
+    }
+
+    public function actionGet()
+    {
+	$logPath = realpath(Yii::$app->basePath . '/web/assets/dataLog');
+	$jsonArray = file($logPath);
+	foreach($jsonArray as $line_num => $line)
+	{
+	    $jsonData = json_decode($line, true);
+	    var_dump($jsonData);
+	}
+    }
+}
