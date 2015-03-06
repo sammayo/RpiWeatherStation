e1f739960457aaf8c0ecf864dcee69f2ade081a3
sammayo888 <sammayo888@gmail.com>
2015-03-06 17:29:46 -0500

    resolved conflict

--- Rpi/config-example.py
+++ Rpi/config-example.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 base_url = 'http://weatherstation/index.php/'
 temp_humidity_pin = 4
 barometer_bus_num = 1
-=======
-argsErrorString = 'Usage: python SensorReader.py dht_pin_number [I2C bus_num]'
-baseUrl = 'http://weatherstation/index.php/'
-bus_num = 1
->>>>>>> ed8e68bf9328ca77f0661f57c90da9ffae41384c
